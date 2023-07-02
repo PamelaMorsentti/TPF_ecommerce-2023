a3ea3a5cdb5ca57759f382f38befca588b987d5e
Pamela Morsentti <pamelamorsentti@gmail.com>
2023-07-02 16:57:58 -0300

    app compila, falta test y doc

---
+++ src/main/java/com/eCommerce/demo/CasoDeUso/ProductoUseCase.java
@@ -0,0 +1,7 @@
+package com.eCommerce.demo.CasoDeUso;
+
+import com.eCommerce.demo.Modelo.DTO.entities.Productos;
+
+public interface ProductoUseCase {
+    Productos create(Productos productos);
+}
--- src/main/java/com/eCommerce/demo/ECommerceApplication.java
+++ src/main/java/com/eCommerce/demo/ECommerceApplication.java
@@ -1,8 +1,13 @@
 package com.eCommerce.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@OpenAPIDefinition(info = @Info(
+		title = "TPF-SpringBoot-Pamela Morsentti",
+		version = "1.0"
+))
 @SpringBootApplication
 public class ECommerceApplication {
 
